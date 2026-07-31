0c1a7abcaa87937348af475c560cc1d95cf74bb1
Beknazar <momynovbeknazar3@gmail.com>
2026-07-31 15:05:11 +0500

    printschedule 1

--- internal/beknazar/beka.go
+++ internal/beknazar/beka.go
@@ -1,1 +1,18 @@
 package beknazar
+
+import "fmt"
+func PrintSchedule (string, busy string, important string) {
+	columns := len(rows[0])
+    border := "+" + strings.Repeat("---------+", columns)
+
+    fmt.Println(border)
+
+    for _, row := range rows {
+        fmt.Print("|")
+
+        for i := 0; i < len(row); i++ {
+            fmt.Print(formatCell(row[i], busy, important))
+            fmt.Print("|")
+        }
+}
+}
