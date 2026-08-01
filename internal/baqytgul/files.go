5521b040d517d7cb75373f1b75893aa9db2af950
Beknazar <momynovbeknazar3@gmail.com>
2026-08-01 15:20:49 +0500

    files.go correction

--- internal/baqytgul/files.go
+++ internal/baqytgul/files.go
@@ -1,11 +1,11 @@
-package main
+package baqytgul
 import "strings"
 const (
     red = "\033[31m"
     yellow = "\033[33m"
     reset = "\033[0m"
 )
-func formatCell(cell byte, busy string, important string) string {
+func FormatCell(cell byte, busy string, important string) string {
 	text := ""
 	switch cell {
     case '0':
