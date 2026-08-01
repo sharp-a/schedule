d7036ee9c7ac2fd5931439f566f8c22f20629f0b
ANuar <zhanna12021977@gmail.com>
2026-08-01 05:17:35 -0700

    fully implemented validation function

---
+++ cmd/schedule/main.go
@@ -0,0 +1,4 @@
+package make
+
+import 
+
--- internal/anuar/printer.go
+++ internal/anuar/printer.go
@@ -1,1 +1,49 @@
 package anuar
+
+import "strconv"
+
+func validateSchedule(rows []string) string {
+
+	switch {
+	case len(rows) == 0 || rows[0] == "":
+		return "error: empty schedule"
+	}
+
+	for index, i := range rows {
+		if index == len(rows)-1 {
+
+		} else {
+			if len(i) == len(rows[index+1]) {
+				continue
+			} else {
+				return "error: rows have different lengths"
+			}
+		}
+	}
+
+	for _, i := range rows {
+		num, error := strconv.Atoi(i)
+
+		if error != nil || num < 0 {
+			return "error: invalid symbol (only 0/1/2 allowed)"
+		}
+
+		for num > 0 {
+			if num % 10 == 0 || num % 10 == 1 || num % 10 == 2 {
+				num /= 10
+			} else {
+				return "error: invalid symbol (only 0/1/2 allowed)"
+			}
+		}
+	}
+
+	return ""
+
+
+}
+
+
+
+// error: empty schedule 
+// error: rows have different lengths
+// error: invalid symbol (only 0/1/2 allowed)
